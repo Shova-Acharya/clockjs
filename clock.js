c8b5e9195c94db5205d4f7277abbb5251593570f
Shova-Acharya <shova.723@gmail.com>
2020-02-11 21:50:34 +0545

    first commit

---
+++ clock.js
@@ -0,0 +1,27 @@
+
+function showTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var session = "AM";
+    
+    if(hour == 0){
+        hour = 24;
+    }
+    
+    if(hour>=13){
+        session = "PM"
+    }
+    
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+    
+    var display = hour + ":" + min + ":" + sec + " "+ session;
+    document.getElementById("MyClockDisplay").textContent = display;
+    setTimeout(showTime, 1000);
+}
+
+
+showTime();
